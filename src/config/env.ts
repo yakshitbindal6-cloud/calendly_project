d5315b28de0308c309c4ef7dc42d09e16cfa6951
Yakshit Bindal <yakshitbindal6@gmail.com>
2026-07-12 13:20:37 +0530

    adding docker compose for temporal

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,3 +3,6 @@ export const PORT = process.env.PORT || 3000;
 export const DATABASE_URL=process.env.DATABASE_URL||'';
 export const Node_env=process.env.Node_env||'development';
 export const slot_Days=Number(process.env.slot_generationDays)||30;
+export const Temporal_Address=process.env.Temporal_Address||'http://localhost:7233';
+export const Temporal_namespace=process.env.Temporal_namespace||'default';
+export const Temporal_taskqueue=process.env.Temporal_taskqueue||'calendly_task';
---
+++ src/config/temporal.ts
@@ -0,0 +1,25 @@
+import {Connection,Client} from"@temporalio/client";
+import {Temporal_Address,Temporal_namespace,Temporal_taskqueue} from"./env.js";
+let client: Client | null = null;
+
+export async function getTemporalClient(){
+    if(client){
+        return client;
+    }
+    const connection = await Connection.connect({
+        address:Temporal_Address,
+    });
+
+     client = new Client({
+        connection,
+        namespace: Temporal_namespace,
+    });
+
+    return client;
+}
+export async function disconnectTemporal() {
+    if(client) {
+        await client.connection.close();
+        client = null;
+    }
+}
